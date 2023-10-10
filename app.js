23880e1f71a677f00187e06a8396659d0bf5382a
Rakib Enam <md.rakib.enam.anik@bracu.ac.bd>
2023-10-10 12:11:41 +0600

    Added passport authentication file

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const ejs = require("ejs");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//base url 
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+// register : get
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+// register : post
+
+// login : get
+
+// login : post
+
+// profile protected route
+
+
+
+
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const express = require("express");
-const app = express();
+require("dotenv").config();
+const app = require("./app");
+
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, ()=> {
+    console.log(`server is running at http://localhost:${PORT}`);
+});
